c547cd796413b28a00a8962ffe3408b2c7505acc
iamcoreyg <iamcoreyg@gmail.com>
2014-04-17 18:42:47 -0700

    Built psimdope from commit a25fd5c on branch master

---
+++ scripts/1906f86b.main.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$(".move-circles").on("click",function(){return!1}),$(".move-circles").on("click",function(){$(".circle").removeClass(".move-circles"),$(".circle").removeClass("initial-circles",1e3,"easeInBack")})});
--- scripts/258597b9.main.js
+++
@@ -1,1 +0,0 @@
-$(document).ready(function(){$(".move-circles").on("mouseenter",function(){$(".circle").removeClass(".move-circles"),$(".circle").removeClass("initial-circles",1e3,"easeInBack")})});
