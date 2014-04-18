155bf107dfd19ec50337d6fd4292ea3c36afed64
iamcoreyg <iamcoreyg@gmail.com>
2014-04-17 19:20:44 -0700

    added nav text

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,8 +2,16 @@ $(document).ready( function() {
     $(".move-circles").on('click', function(){
         return false
     })
+    var navIsOpen = false
     $(".move-circles").on('click', function() {
         $(".circle").removeClass(".move-circles")
         $(".circle").removeClass("initial-circles", 1000, "easeInBack" )
+        navIsOpen = true
     })
+
+    $(".circle a").on('mouseenter', function() {
+        if(navIsOpen)
+            $("#nav-title").text( $(this).data("nav") )
+    })
+
 })
