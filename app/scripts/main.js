a25fd5c6678c313708c04f69cc0b156449c0fd02
iamcoreyg <iamcoreyg@gmail.com>
2014-04-17 18:42:30 -0700

    circle is on click

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,8 @@
 $(document).ready( function() {
-    $(".move-circles").on('mouseenter', function() {
+    $(".move-circles").on('click', function(){
+        return false
+    })
+    $(".move-circles").on('click', function() {
         $(".circle").removeClass(".move-circles")
         $(".circle").removeClass("initial-circles", 1000, "easeInBack" )
     })
